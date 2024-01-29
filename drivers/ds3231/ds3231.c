559365c5f6830084649983e17ac6a5e8eb6d0b93
Anuj Deshpande <anuj@makerville.io>
2024-01-29 18:17:12 +0530

    Add board file, prj.conf

--- drivers/ds3231/ds3231.c
+++ drivers/ds3231/ds3231.c
@@ -3,5 +3,9 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/rtc.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/sys/util.h>
 
-#include "ds3231.h"
--- drivers/ds3231/ds3231.h
+++
@@ -1,7 +0,0 @@
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-
-#ifndef DS3231_H
-#define DS3231_H
-
-#endif

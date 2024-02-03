81bd2918f2c3f75ad0d941d2533df9e28ad4e129
Anuj Deshpande <anuj@makerville.io>
2024-02-03 18:45:42 +0530

    Add makefile and logging feature to simple app

--- examples/simple/src/main.c
+++ examples/simple/src/main.c
@@ -1,37 +1,40 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(simple_app);
 /*
  * Get a device structure from a devicetree node with compatible
  * "bosch,bme280". (If there are multiple, just pick one.)
  */
-static const struct device *get_ds3231_device(void)
-{
-	const struct device *const dev = DEVICE_DT_GET_ANY(ds3231);
+/* static const struct device *get_ds3231_device(void) */
+/* { */
+/* 	const struct device *const dev = DEVICE_DT_GET_ANY(ds3231); */
 
-	if (dev == NULL) {
-		/* No such node, or the node does not have status "okay". */
-		printk("\nError: no device found.\n");
-		return NULL;
-	}
+/* 	if (dev == NULL) { */
+/* 		/\* No such node, or the node does not have status "okay". *\/ */
+/* 		printk("\nError: no device found.\n"); */
+/* 		return NULL; */
+/* 	} */
 
-	if (!device_is_ready(dev)) {
-		printk("\nError: Device \"%s\" is not ready; "
-		       "check the driver initialization logs for errors.\n",
-		       dev->name);
-		return NULL;
-	}
+/* 	if (!device_is_ready(dev)) { */
+/* 		printk("\nError: Device \"%s\" is not ready; " */
+/* 		       "check the driver initialization logs for errors.\n", */
+/* 		       dev->name); */
+/* 		return NULL; */
+/* 	} */
 
-	printk("Found device \"%s\", getting time\n", dev->name);
-	return dev;
-}
+/* 	printk("Found device \"%s\", getting time\n", dev->name); */
+/* 	return dev; */
+/* } */
 
 int main(void)
 {
-	const struct device *dev = get_ds3231_device();
+        LOG_INF("Running DS3231 simple app on %s\n", CONFIG_BOARD);
+	/* const struct device *dev = get_ds3231_device(); */
 
-	if (dev == NULL) {
-		return 0;
-	}
+	/* if (dev == NULL) { */
+	/* 	return 0; */
+	/* } */
 
-	while (1) {
-	}
 	return 0;
 }

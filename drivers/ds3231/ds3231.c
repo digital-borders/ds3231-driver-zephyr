56932fbf813fc503a394c14f8ca3bf111fd3d849
Anuj Deshpande <anuj@makerville.io>
2024-02-06 00:09:47 +0530

    Add minimal driver code with get_time api

--- drivers/ds3231/ds3231.c
+++ drivers/ds3231/ds3231.c
@@ -53,9 +53,9 @@ LOG_MODULE_REGISTER(ds3231, CONFIG_RTC_LOG_LEVEL);
 #define DS3231_DATE_MASK               GENMASK(3, 0)
 #define DS3231_MONTH_CENTURY           BIT(7)
 #define DS3231_MONTH_10                BIT(4)
-#define DS3231_MONTH_MASK              GENMASK(3, 0)
+#define DS3231_MONTHS_MASK              GENMASK(3, 0)
 #define DS3231_YEAR_10                 GENMASK(7, 4)
-#define DS3231_YEAR_MASK               GENMASK(3, 0)
+#define DS3231_YEARS_MASK               GENMASK(3, 0)
 #define DS3231_ALARM_1_SECONDS_A1M1    BIT(7)
 #define DS3231_ALARM_1_SECONDS_10      GENMASK(6, 4)
 #define DS3231_ALARM_1_SECONDS_SECONDS GENMASK(3, 0)
@@ -105,3 +105,127 @@ LOG_MODULE_REGISTER(ds3231, CONFIG_RTC_LOG_LEVEL);
 #define DS3231_TEMP_MSB_SIGN     BIT(7)
 #define DS3231_TEMP_MSB_DATA     GENMASK(6, 0)
 #define DS3231_TEMP_LSB_DATA     GENMASK(7, 6)
+
+/* RTC alarm time fields supported by DS3231 */
+#define DS3231_RTC_ALARM_1_TIME_MASK                                                               \
+	(RTC_ALARM_TIME_MASK_SECOND | RTC_ALARM_TIME_MASK_MINUTE | RTC_ALARM_TIME_MASK_HOUR |      \
+	 RTC_ALARM_TIME_MASK_WEEKDAY | RTC_ALARM_TIME_MASK_MONTHDAY)
+
+#define DS3231_RTC_ALARM_2_TIME_MASK                                                               \
+	(RTC_ALARM_TIME_MASK_MINUTE | RTC_ALARM_TIME_MASK_HOUR | RTC_ALARM_TIME_MASK_WEEKDAY |     \
+	 RTC_ALARM_TIME_MASK_MONTHDAY)
+
+/* The DS3231 enumerates months 1 to 12, RTC API uses 0 to 11 */
+#define DS3231_MONTHS_OFFSET 1
+
+/* The DS3231 only supports two-digit years, calculate offset to use */
+#define DS3231_YEARS_OFFSET (2000 - 1900)
+
+struct ds3231_config {
+	const struct i2c_dt_spec i2c;
+	// todo add interrupt pin, etc.
+};
+struct ds3231_data {
+	struct k_mutex lock;
+};
+
+static int ds3231_read_regs(const struct device *dev, uint8_t addr, void *buf, size_t len)
+{
+	const struct ds3231_config *config = dev->config;
+	int err;
+
+	err = i2c_write_read_dt(&config->i2c, &addr, sizeof(addr), buf, len);
+	if (err != 0) {
+		LOG_ERR("failed to read reg addr 0x%02x, len %d (err %d)", addr, len, err);
+		return err;
+	}
+
+	return 0;
+}
+
+static int ds3231_read_reg8(const struct device *dev, uint8_t addr, uint8_t *val)
+{
+	return ds3231_read_regs(dev, addr, val, sizeof(*val));
+}
+
+static int ds3231_write_regs(const struct device *dev, uint8_t addr, void *buf, size_t len)
+{
+	const struct ds3231_config *config = dev->config;
+	uint8_t block[sizeof(addr) + len];
+	int err;
+
+	block[0] = addr;
+	memcpy(&block[1], buf, len);
+
+	err = i2c_write_dt(&config->i2c, block, sizeof(block));
+	if (err != 0) {
+		LOG_ERR("failed to write reg addr 0x%02x, len %d (err %d)", addr, len, err);
+		return err;
+	}
+
+	return 0;
+}
+
+static int ds3231_write_reg8(const struct device *dev, uint8_t addr, uint8_t val)
+{
+	return ds3231_write_regs(dev, addr, &val, sizeof(val));
+}
+
+static int ds3231_get_time(const struct device *dev, struct rtc_time *timeptr)
+{
+	uint8_t regs[10];
+	int err;
+
+	/* Read registers DS3231_CONTROL_1 through DS3231_YEARS */
+	/* err = ds3231_read_regs(dev, DS3231_CONTROL_1, &regs, sizeof(regs)); */
+	/* if (err != 0) { */
+	/* 	return err; */
+	/* } */
+
+	/* if ((regs[0] & DS3231_CONTROL_1_STOP) != 0) { */
+	/* 	LOG_WRN("time circuits frozen"); */
+	/* 	return -ENODATA; */
+	/* } */
+
+	/* if ((regs[3] & DS3231_SECONDS_OS) != 0) { */
+	/* 	LOG_WRN("oscillator stopped or interrupted"); */
+	/* 	return -ENODATA; */
+	/* } */
+
+	memset(timeptr, 0U, sizeof(*timeptr));
+	timeptr->tm_sec = bcd2bin(regs[3] & DS3231_SECONDS_MASK);
+	timeptr->tm_min = bcd2bin(regs[4] & DS3231_MINUTES_MASK);
+	timeptr->tm_hour = bcd2bin(regs[5] & DS3231_HOURS_MASK);
+	timeptr->tm_mday = bcd2bin(regs[6] & DS3231_DAYS_MASK);
+	timeptr->tm_wday = bcd2bin(regs[7] & DS3231_DAYS_MASK);
+	timeptr->tm_mon = bcd2bin(regs[8] & DS3231_MONTHS_MASK) - DS3231_MONTHS_OFFSET;
+	timeptr->tm_year = bcd2bin(regs[9] & DS3231_YEARS_MASK) + DS3231_YEARS_OFFSET;
+	timeptr->tm_yday = -1;
+	timeptr->tm_isdst = -1;
+
+	LOG_DBG("get time: year = %d, mon = %d, mday = %d, wday = %d, hour = %d, "
+		"min = %d, sec = %d",
+		timeptr->tm_year, timeptr->tm_mon, timeptr->tm_mday, timeptr->tm_wday,
+		timeptr->tm_hour, timeptr->tm_min, timeptr->tm_sec);
+
+	return 0;
+}
+static const struct rtc_driver_api ds3231_driver_api = {
+	.get_time = ds3231_get_time,
+};
+
+static int ds3231_init(const struct device* dev)
+{
+	
+}
+
+#define DS3231_INIT(inst)                                                                          \
+	static const struct ds3231_config ds3231_config_##inst = {                                 \
+		.i2c = I2C_DT_SPEC_INST_GET(inst),                                                 \
+	};                                                                                         \
+	static struct ds3231_data ds3231_data_##inst;                                              \
+	DEVICE_DT_INST_DEFINE(inst, &ds3231_init, NULL, &ds3231_data_##inst,                       \
+			      &ds3231_config_##inst, POST_KERNEL, CONFIG_RTC_INIT_PRIORITY,        \
+			      &ds3231_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(DS3231_INIT)
--- examples/simple/src/main.c
+++ examples/simple/src/main.c
@@ -1,36 +1,34 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/rtc.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(simple_app);
-/*
- * Get a device structure from a devicetree node with compatible
- * "bosch,bme280". (If there are multiple, just pick one.)
- */
-/* static const struct device *get_ds3231_device(void) */
-/* { */
-/* 	const struct device *const dev = DEVICE_DT_GET_ANY(ds3231); */
 
-/* 	if (dev == NULL) { */
-/* 		/\* No such node, or the node does not have status "okay". *\/ */
-/* 		printk("\nError: no device found.\n"); */
-/* 		return NULL; */
-/* 	} */
+static const struct device *get_ds3231_device(void)
+{
+	const struct device *const dev = DEVICE_DT_GET_ANY(ds3231);
 
-/* 	if (!device_is_ready(dev)) { */
-/* 		printk("\nError: Device \"%s\" is not ready; " */
-/* 		       "check the driver initialization logs for errors.\n", */
-/* 		       dev->name); */
-/* 		return NULL; */
-/* 	} */
+	if (dev == NULL) {
+		/* No such node, or the node does not have status "okay". */
+		printk("\nError: no device found.\n");
+		return NULL;
+	}
 
-/* 	printk("Found device \"%s\", getting time\n", dev->name); */
-/* 	return dev; */
-/* } */
+	if (!device_is_ready(dev)) {
+		printk("\nError: Device \"%s\" is not ready; "
+		       "check the driver initialization logs for errors.\n",
+		       dev->name);
+		return NULL;
+	}
+
+	printk("Found device \"%s\", getting time\n", dev->name);
+	return dev;
+}
 
 int main(void)
 {
         LOG_INF("Running DS3231 simple app on %s\n", CONFIG_BOARD);
-	/* const struct device *dev = get_ds3231_device(); */
+	const struct device *dev = get_ds3231_device();
 
 	/* if (dev == NULL) { */
 	/* 	return 0; */

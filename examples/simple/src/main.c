30f4233290cad3776e91cd558f3a9ccf47baf305
Anuj Deshpande <anuj@makerville.io>
2024-02-13 16:24:35 +0530

    Use zephyr amend instead of adding as a new lib

--- examples/simple/src/main.c
+++ examples/simple/src/main.c
@@ -2,11 +2,12 @@
 #include <zephyr/drivers/rtc.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
+
 LOG_MODULE_REGISTER(simple_app);
 
 static const struct device *get_ds3231_device(void)
 {
-	const struct device *const dev = DEVICE_DT_GET_ANY(ds3231);
+	const struct device *const dev = DEVICE_DT_GET_ANY(analogdevices_ds3231);
 
 	if (dev == NULL) {
 		/* No such node, or the node does not have status "okay". */
@@ -27,12 +28,13 @@ static const struct device *get_ds3231_device(void)
 
 int main(void)
 {
-        LOG_INF("Running DS3231 simple app on %s\n", CONFIG_BOARD);
+	LOG_INF("Running DS3231 simple app on %s\n", CONFIG_BOARD);
 	const struct device *dev = get_ds3231_device();
-
-	/* if (dev == NULL) { */
-	/* 	return 0; */
-	/* } */
-
+	if (dev == NULL) {
+		return 0;
+	}
+	struct rtc_time get_t;
+	rtc_get_time(dev, &get_t);
+	LOG_INF("Current date/time is %d-%d-%d   %d:%d:%d\n", get_t.tm_year + 1900, get_t.tm_mon + 1, get_t.tm_mday, get_t.tm_hour, get_t.tm_min, get_t.tm_sec);
 	return 0;
 }

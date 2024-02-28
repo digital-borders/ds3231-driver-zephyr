4de96a51ce62a2bc2d7d4c9b071d43d1b357c8f8
Anuj Deshpande <anuj@makerville.io>
2024-02-28 12:51:12 +0530

    Cleanup simple app

--- examples/simple/src/main.c
+++ examples/simple/src/main.c
@@ -31,14 +31,15 @@ static const struct device *get_ds3231_device(void)
 
 int main(void)
 {
-	LOG_INF("Running DS3231 shell app on %s\n", CONFIG_BOARD);
+	time_t timer_set = 1708955166;
+	struct rtc_time set_t, get_t;
+
+	LOG_INF("Running DS3231 simple  app on %s\n", CONFIG_BOARD);
 	dev = get_ds3231_device();
 	if (dev == NULL) {
 		return 0;
 	}
 
-	time_t timer_set = 1708955166;
-	struct rtc_time set_t;
 	gmtime_r(&timer_set, (struct tm *)(&set_t));
 	LOG_INF("Setting date/time is %d-%d-%d %d  %d:%d:%d\n", set_t.tm_year + 1900,
 		set_t.tm_mon + 1, set_t.tm_mday, set_t.tm_wday, set_t.tm_hour, set_t.tm_min,
@@ -51,14 +52,12 @@ int main(void)
 
 	k_msleep(2000);
 
-	struct rtc_time get_t;
 	ret = rtc_get_time(dev, &get_t);
 	if (ret != 0) {
-		LOG_INF("Error setting time!!");
-	}
-	else{
-		LOG_INF("Date/time is %d-%d-%d   %d:%d:%d\n", get_t.tm_year + 1900, get_t.tm_mon + 1,
-			get_t.tm_mday, get_t.tm_hour, get_t.tm_min, get_t.tm_sec);
+		LOG_INF("Error getting time!!");
+	} else {
+		LOG_INF("Date/time is %d-%d-%d   %d:%d:%d\n", get_t.tm_year + 1900,
+			get_t.tm_mon + 1, get_t.tm_mday, get_t.tm_hour, get_t.tm_min, get_t.tm_sec);
 	}
 	return 0;
 }
